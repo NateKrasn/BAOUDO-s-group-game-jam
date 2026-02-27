c70efdf713778b675c53cee14d9c40f0133b43dd
Unlocked Studios <natekrasn@gmail.com>
2026-02-26 21:20:43 -0500

    Revert "UI fully setup plus fonts and new enemies"
    This reverts commit 661ac6a227909d9f23f8fbeb97723b4d879b596f.

--- new-game-project/enemy spawning script.gd
+++ new-game-project/enemy spawning script.gd
@@ -1,21 +1,16 @@
 extends Path2D
 
 var DEER = preload("res://test enemy.tscn")
-var wolf = preload("res://wolf.tscn")
-var cheetah = preload("res://cheeta.tscn")
-var bat = preload("res://bat.tscn")
-
-
+var speed = 100
 
 func _ready() -> void:
+	spawn_enemies()
 	await get_tree().create_timer(2).timeout
 	spawn_enemies()
 	await get_tree().create_timer(2).timeout
-	spawn_wolf()
+	spawn_enemies()
 	await get_tree().create_timer(2).timeout
-	spawn_cheetah()
-	await get_tree().create_timer(2).timeout
-	spawn_bat()
+	spawn_enemies()
 	await get_tree().create_timer(2).timeout
 	spawn_enemies()
 	await get_tree().create_timer(2).timeout
@@ -25,16 +20,3 @@ func _ready() -> void:
 func spawn_enemies():
 	var new_DEER = DEER.instantiate()
 	add_child(new_DEER)
-
-func spawn_wolf():
-	var new_wolf = wolf.instantiate()
-	add_child(new_wolf)
-
-func spawn_cheetah():
-	var new_cheetah = cheetah.instantiate()
-	add_child(new_cheetah)
-
-
-func spawn_bat():
-	var new_bat = bat.instantiate()
-	add_child(new_bat)
--- new-game-project/visuals for tower UI and UI/main_ui.gd
+++ new-game-project/visuals for tower UI and UI/main_ui.gd
@@ -1,11 +1,10 @@
 extends Control
 
 var pressed_alr: bool
-var pressed_alr2: bool
 
 func _ready() -> void:
 	pressed_alr = false
-	pressed_alr2 = true
+
 
 func _on_buy_button_pressed() -> void:
 	if not pressed_alr:
@@ -14,17 +13,3 @@ func _on_buy_button_pressed() -> void:
 	elif pressed_alr:
 		$"placing panel".visible = true
 		pressed_alr = false
-
-
-func _on_button_pressed() -> void:
-	$"main menu show".visible = true
-	get_tree().paused = true
-
-
-func _on_button_2_pressed() -> void:
-	$"main menu show".visible = false
-	get_tree().paused = false
-
-
-func _on_quit_pressed() -> void:
-	get_tree().change_scene_to_file("res://main_menu.tscn")
